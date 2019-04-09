f124a1fd76087b9478da612e254967bfa8cf6d86
sureshbandi155 <43289827+sureshbandi155@users.noreply.github.com>
2019-04-09 15:46:36 +0530

    added

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// var arrowTop = document.querySelector('#toparrow');
+// console.log(arrowTop);
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     arrowTop.style.display = "inline";
+//   } else {
+//     arrowTop.style.display = "none";
+//   }
+// }
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
